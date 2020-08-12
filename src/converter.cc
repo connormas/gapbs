9cea905bed21a84576d923afb72a0cc78358f49d
Connor Masterson <connormasterson@Connors-MacBook-Air.local>
2020-08-11 19:47:11 -0700

    cleaned up code

--- src/converter.cc
+++ src/converter.cc
@@ -15,24 +15,6 @@ using namespace std;
 int main(int argc, char* argv[]) {
   CLConvert cli(argc, argv, "converter");
   cli.ParseArgs();
-  
-  /*int* arr = new (int[67108864]);
-  std::cout << "size of arr: " << sizeof(*arr)  << " addresss: " << arr << std::endl;
-  for(int i = 0; i < (67108864); i++){
-    arr[i] = 0;
-  }
-  std::cout << "size of arr: " << sizeof(*arr) << " addresss: " << arr << std::endl;
-  std::realloc(arr, 67108864/2);
-  if(arr == nullptr){
-    std::cout << "its null! realloc failed\n";
-  }
-  for(int i = 0; i < (67108864/2); i++){
-    arr[i] = 0;
-  }
-  std::cout << "size of arr: " << sizeof(*arr) << " addresss: " << arr << std::endl;
-  delete arr;
-  exit(0);*/
-
   if (cli.out_weighted()) {
     WeightedBuilder bw(cli);
     WGraph wg = bw.MakeGraph(cli.lowmem());
