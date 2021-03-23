145993a15f45b2f7b77370771504ca9072246400
Connor Curran Masterson <ccmaster@kaffirlime.soe.ucsc.edu>
2021-03-23 12:48:05 -0700

    added urand to script and new urand results

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+################################################################
+# INITIAL BOOKKEEPING AND ENVIRONMENT VARIABLES
+################################################################
+# Author: Connor Masterson
+# This script runs both Gauss-Seidell and Jacobi style pagerank
+# on Kron and Urand graphs at varying scales.
+
 GAPBS_DIR=/soe/ccmaster/gapbs
 GRAPH_DIR=/lscratch/graphs
 OUT_DIR=$GAPBS_DIR/output/run.sh-results
@@ -12,7 +19,10 @@ export GOMP_CPU_AFFINITY="0-23"
 PRE="numactl -N 0 -m 0"
 # PRE="numactl --interleave=all"
 
-# make output files
+################################################################
+# TEST FOR KRON GRAPHS
+################################################################
+
 gskout="$OUT_DIR/GS-kron-$THREADS.out"
 jkout="$OUT_DIR/J-kron-$THREADS.out"
 if [ -e $gskout ]; then
@@ -30,14 +40,39 @@ echo "$gskout"
 echo "$jkout"
 
 echo "Now doing..."
-for SCALE in 17 18 19 20  21 22 23 24 25 26 27 28; do
-  
-  echo "kron-$SCALE"
-  
+for SCALE in 17 18 19 20 21 22 23 24 25 26 27 28; do
+  echo "kron-$SCALE"  
   echo "SCALE: $SCALE" >> $gskout
   echo "SCALE: $SCALE" >> $jkout
   $GAPBS_DIR/prgs -g $SCALE -i 5 | grep -B 2 "Average Time" >> $gskout
   $GAPBS_DIR/prj  -g $SCALE -i 5 | grep -B 2 "Average Time" >> $jkout
-
 done
 
+################################################################
+# TEST FOR URAND GRAPHS
+################################################################
+
+gskout="$OUT_DIR/GS-urand-$THREADS.out"
+jkout="$OUT_DIR/J-urand-$THREADS.out"
+if [ -e $gskout ]; then
+  rm $gskout
+fi                                                                              
+touch $gskout
+if [ -e $jkout ]; then
+  rm $jkout
+fi                                                                              
+touch $jkout
+
+echo "Starting URAND runs scale 17-28"
+echo "All results will be in"
+echo "$gskout" 
+echo "$jkout"
+
+echo "Now doing..."
+for SCALE in 17 18 19 20 21 22 23 24 25 26 27 28; do
+  echo "urand-$SCALE"  
+  echo "SCALE: $SCALE" >> $gskout
+  echo "SCALE: $SCALE" >> $jkout
+  $GAPBS_DIR/prgs -u $SCALE -i 5 | grep -B 2 "Average Time" >> $gskout
+  $GAPBS_DIR/prj  -u $SCALE -i 5 | grep -B 2 "Average Time" >> $jkout
+done
